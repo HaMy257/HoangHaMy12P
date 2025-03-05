d1e65afeab2c37c39716ed78ca91eab9f7507695
HaMy257 <hoanghamy2507@gmail.com>
2025-03-05 09:09:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login()
 {
   let a = username.value
   let b = password.value
-  if (a == "HoangHaMy" && b == "25072007")
+  if (a == "abc" && b == "123")
   {
     window.open("Home.html")
     document.getElememtById("compiler").style.display="block";
